deac3869a5307505ab2132f1064001488b7e502f
alucard <faalsaru@gmail.com>
2021-09-14 12:23:57 -0500

    create, delete and update todos

--- server/src/controller/index.controller.ts
+++ server/src/controller/index.controller.ts
@@ -15,3 +15,45 @@ export const getAllTodos = async (req: Request, res: Response) => {
         console.error('An error has occurred in getAllTodos query: ' + (error as Error).message);
     };
 };
+
+export const createTodo = async (req: Request, res: Response) => {
+    const { description } = req.body;
+    try {
+        await pool.query(`INSERT INTO todo (description) VALUES ('${description}');`);
+        res.status(200).json({
+            message: 'Todo created successfully',
+            body: {
+                todo: {
+                    description,
+                },
+            },
+        });
+    } catch(error: unknown) {
+        console.error('An error has occurred in createTodo query: ' + (error as Error).message);
+    };
+};
+
+export const deleteTodo = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    try {
+        await pool.query(`DELETE FROM todo WHERE todo_id=${id};`);
+        res.status(200).json({
+            message: `Todo ${id} deleted successfully`,
+        });
+    } catch(error: unknown) {
+        console.error('An error has occurred in deleteTodo query: ' + (error as Error).message);
+    };
+};
+
+export const updateTodo = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const { description } = req.body;
+    try {
+        await pool.query(`UPDATE todo SET description='${description}' WHERE todo_id=${id};`);
+        res.status(200).json({
+            message: `Todo ${id} updated successfully`,
+        });
+    } catch(error: unknown) {
+        console.error('An error has occurred in updateTodo query: ' + (error as Error).message);
+    };
+};
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
-import { root, getAllTodos } from '../controller/index.controller';
+import { root, getAllTodos, createTodo, deleteTodo, updateTodo } from '../controller/index.controller';
 
 router.get('/', root);
 router.get('/todos', getAllTodos);
+router.post('/todos', createTodo);
+router.delete('/todos/:id', deleteTodo);
+router.put('/todos/:id', updateTodo);
 
 export default router;
